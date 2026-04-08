ebe65a37ed2f285ae3486353c123a92e6ff990c5
vladik-png <vladsemotuk42@gmail.com>
2026-04-08 19:45:28 +0300

    refactoring code

--- go.mod
+++ go.mod
@@ -24,11 +24,17 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sts v1.41.5 // indirect
 	github.com/aws/smithy-go v1.24.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	gorm.io/driver/sqlite v1.6.0 // indirect
+	gorm.io/gorm v1.31.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,95 +1,26 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"log"
-	"net"
 	"net/http"
-	"os"
-	"sync"
-
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/ec2"
-	"github.com/joho/godotenv"
-)
-
-var (
-	ec2Client     *ec2.Client
-	latestMetrics = make(map[string]map[string]interface{})
-	metricsMu     sync.Mutex
-)
-
-
-const (
-	LocalInstanceID = "my-windows-server"
-	LocalMacAddress = "00:11:22:33:44:55"
-	LocalAgentURL   = "http://127.0.0.1:8081"
 )
 
 func main() {
-	godotenv.Load()
-	region := os.Getenv("AWS_DEFAULT_REGION")
-	if region == "" {
-		region = "eu-north-1"
-	}
+	initConfig()
+	initDB()
 
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
-	if err != nil {
-		log.Println("No AWS config found (will work only locally):", err)
-	} else {
-		ec2Client = ec2.NewFromConfig(cfg)
-	}
+	go monitorServers()
 
-	http.HandleFunc("/", enableCORS(index))
+	http.HandleFunc("/", enableCORS(getInstances))
+	http.HandleFunc("/logs", enableCORS(getLogs))
+	http.HandleFunc("/alerts", enableCORS(getAlerts))
 	http.HandleFunc("/system-metrics", enableCORS(getMetricsForFront))
 	http.HandleFunc("/report-metrics", enableCORS(receiveMetricsFromAgent))
 	http.HandleFunc("/start", enableCORS(startInstance))
 	http.HandleFunc("/stop", enableCORS(stopInstance))
 
-	log.Println("Master Server started on port :8082")
-	http.ListenAndServe(":8082", nil)
-}
-
-// Wake-on-LAN функція
-func wakeOnLan(macAddr string) error {
-	hwAddr, err := net.ParseMAC(macAddr)
-	if err != nil {
-		return err
-	}
-	packet := make([]byte, 102)
-	copy(packet[0:6], []byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF})
-	for i := 1; i <= 16; i++ {
-		copy(packet[i*6:i*6+6], hwAddr)
-	}
-	conn, err := net.Dial("udp", "255.255.255.255:9")
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	_, err = conn.Write(packet)
-	return err
-}
-
-func receiveMetricsFromAgent(w http.ResponseWriter, r *http.Request) {
-	var data map[string]interface{}
-	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	id, _ := data["instance_id"].(string)
-
-	metricsMu.Lock()
-	latestMetrics[id] = data
-	metricsMu.Unlock()
-	w.WriteHeader(http.StatusOK)
-}
-
-func getMetricsForFront(w http.ResponseWriter, r *http.Request) {
-	metricsMu.Lock()
-	defer metricsMu.Unlock()
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(latestMetrics)
+	log.Println("🚀 Master Server started on port :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func enableCORS(next http.HandlerFunc) http.HandlerFunc {
@@ -103,72 +34,4 @@ func enableCORS(next http.HandlerFunc) http.HandlerFunc {
 		}
 		next(w, r)
 	}
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	instances := make([]map[string]interface{}, 0)
-
-	instances = append(instances, map[string]interface{}{
-		"InstanceId": LocalInstanceID,
-		"Provider":   "Local",
-
-	})
-
-	if ec2Client != nil {
-		resp, err := ec2Client.DescribeInstances(context.TODO(), &ec2.DescribeInstancesInput{})
-		if err == nil {
-			for _, res := range resp.Reservations {
-				for _, inst := range res.Instances {
-					instances = append(instances, map[string]interface{}{
-						"InstanceId": *inst.InstanceId,
-						"Provider":   "AWS",
-						"State":      inst.State.Name,
-					})
-				}
-			}
-		}
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(instances)
-}
-
-func startInstance(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
-
-	if id == LocalInstanceID {
-		err := wakeOnLan(LocalMacAddress)
-		if err != nil {
-			http.Error(w, "Error sending Wake-on-LAN packet: "+err.Error(), 500)
-			return
-		}
-		log.Println("Wake-on-LAN packet sent to", LocalMacAddress)
-	} else if ec2Client != nil {
-		_, err := ec2Client.StartInstances(context.TODO(), &ec2.StartInstancesInput{InstanceIds: []string{id}})
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-	}
-	w.WriteHeader(http.StatusOK)
-}
-
-func stopInstance(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
-
-	if id == LocalInstanceID {
-		_, err := http.Get(LocalAgentURL + "/shutdown")
-		if err != nil {
-			http.Error(w, "Failed to connect to agent: "+err.Error(), 500)
-			return
-		}
-		log.Println("Shutdown command sent to local server")
-	} else if ec2Client != nil {
-		_, err := ec2Client.StopInstances(context.TODO(), &ec2.StopInstancesInput{InstanceIds: []string{id}})
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-	}
-	w.WriteHeader(http.StatusOK)
 }
